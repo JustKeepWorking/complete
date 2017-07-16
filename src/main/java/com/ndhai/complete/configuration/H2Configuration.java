55e3ac531bdd678f8f6b7eba6eb90bc5d9425cc8
Hai Nguyen <1533478@hcmut.edu.vn>
2017-07-16 21:33:33 +0700

    add new H2 configuration

---
+++ src/main/java/com/ndhai/complete/configuration/H2Configuration.java
@@ -0,0 +1,29 @@
+package com.ndhai.complete.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by nduyhai on 7/16/2017.
+ */
+@Configuration
+public class H2Configuration {
+    @Bean
+    public DataSource H2DataSource() {
+        // no need shutdown, EmbeddedDatabaseFactoryBean will take care of this
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.H2).build();
+        return db;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(H2DataSource());
+    }
+}
