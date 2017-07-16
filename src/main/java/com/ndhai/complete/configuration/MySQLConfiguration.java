720405e722a09bd5e7c4af7038e710767d44f538
Hai Nguyen <1533478@hcmut.edu.vn>
2017-07-16 20:21:40 +0700

    first commit

---
+++ src/main/java/com/ndhai/complete/configuration/MySQLConfiguration.java
@@ -0,0 +1,44 @@
+package com.ndhai.complete.configuration;
+
+import org.apache.tomcat.jdbc.pool.DataSourceProxy;
+import org.apache.tomcat.jdbc.pool.jmx.ConnectionPool;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * Created by nduyhai on 7/16/2017.
+ */
+
+@Configuration
+public class MySQLConfiguration {
+
+    @Bean
+    @ConditionalOnExpression("${app.datasource.mysql.jmxEnabled:true}")
+    public ConnectionPool getMySQLJmxPool(@Qualifier("MySQLDataSource") DataSource dataSource) throws SQLException {
+        return ((DataSourceProxy)dataSource).createPool().getJmxPool();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "app.datasource.mysql")
+    public DataSource MySQLDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public PlatformTransactionManager MySQLTransactionManager() {
+        return new DataSourceTransactionManager(MySQLDataSource());
+    }
+
+
+
+
+}
---
+++ src/main/java/com/ndhai/complete/configuration/PostgreSQLConfiguration.java
@@ -0,0 +1,43 @@
+package com.ndhai.complete.configuration;
+
+import org.apache.tomcat.jdbc.pool.DataSourceProxy;
+import org.apache.tomcat.jdbc.pool.jmx.ConnectionPool;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * Created by nduyhai on 7/16/2017.
+ */
+@Configuration
+public class PostgreSQLConfiguration {
+
+    @Bean
+    @ConditionalOnExpression("${app.datasource.postgresql.jmxEnabled:true}")
+    public ConnectionPool getPostgreSQLJmxPool(@Qualifier("PostgreSQLDataSource") DataSource dataSource) throws SQLException {
+        return ((DataSourceProxy)dataSource).createPool().getJmxPool();
+    }
+
+
+    @Primary
+    @Bean
+    @ConfigurationProperties(prefix = "app.datasource.postgresql")
+    public DataSource PostgreSQLDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager PostgreSQLTransactionManager() {
+        return new DataSourceTransactionManager(PostgreSQLDataSource());
+    }
+}
