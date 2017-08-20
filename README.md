61b4340989c8c8f7ad834927b24dc9b2f2a9ea1e
Hai Nguyen <1533478@hcmut.edu.vn>
2017-08-20 12:44:21 +0700

    add services register discovery eureka server

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # complete
+
+## data
+
+App about connect with database
+
+## data-docs
+
+Sample with Swagger 2
+
+## config-server
+
+Sample with configuration server with spring cloud config server
+
+## config-client
+
+Sample with config client and eureka client (register services)
+
+
+## security
+
+Sample about custom security ldap
+
--- config-client/src/main/java/com/ndhai/comlete/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/ndhai/comlete/configclient/ConfigClientApplication.java
@@ -2,12 +2,14 @@ package com.ndhai.comlete.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  *
  * @author nduyhai
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
